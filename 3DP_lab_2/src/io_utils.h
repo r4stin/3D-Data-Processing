042b2981d8bba2a707019883c4c710761a92d1f1
Rastin <r.akbari625@gmail.com>
2024-05-18 14:44:40 +0200

    "Adding complete version of 3DP_Lab_2"

--- 3DP_lab_2/src/io_utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <string>
-#include <opencv2/opencv.hpp>
-
-bool readFileNamesFromFolder ( const std::string& input_folder_name, std::vector< std::string >& names );
-bool loadCameraParams( const std::string &file_name, cv::Size &image_size,
-                       cv::Mat &camera_matrix, cv::Mat &dist_coeffs );
